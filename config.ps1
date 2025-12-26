13b3567b0ba674d7fa8a27897a659cb5deeb0d5a
Jerry <jerryleerico@hotmail.com>
2025-12-26 03:15:51 -0800

    Customize config and ASCII art

---
+++ config.ps1
@@ -0,0 +1,19 @@
+$Config = @{
+    Username  = "jerryrico"
+    BirthYear = 1988
+
+    Profile = [ordered]@{
+        OS                      = "Windows 11"
+        Uptime                  = $null
+        Host                    = "N/A"
+        Kernel                  = "Network Automation Engineer"
+        IDE                     = "VS Code"
+        "Languages.Programming" = "Python"
+        Hobbies                 = "Networking labs, automation, learning"
+    }
+
+    Contact = [ordered]@{
+        LinkedIn = "jerryrico"
+        GitHub   = "NetworkEngCode"
+    }
+}
